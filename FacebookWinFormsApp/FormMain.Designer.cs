44e73b20933668aa77d150f896c608322597ec3d
Itay Finkelshtein <itayf9@gmail.com>
2023-08-01 18:21:07 +0300

    Add project files.

---
+++ FacebookWinFormsApp/FormMain.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace BasicFacebookFeatures
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.tabControlApp = new System.Windows.Forms.TabControl();
+            this.tabPageProfile = new System.Windows.Forms.TabPage();
+            this.pictureBoxProfile = new System.Windows.Forms.PictureBox();
+            this.tabPageFeatures = new System.Windows.Forms.TabPage();
+            this.pictureBoxCoverPhoto = new System.Windows.Forms.PictureBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.tabControlApp.SuspendLayout();
+            this.tabPageProfile.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCoverPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(18, 17);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(268, 32);
+            this.buttonLogin.TabIndex = 36;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Enabled = false;
+            this.buttonLogout.Location = new System.Drawing.Point(18, 57);
+            this.buttonLogout.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(268, 32);
+            this.buttonLogout.TabIndex = 52;
+            this.buttonLogout.Text = "Logout";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
+            // 
+            // tabControlApp
+            // 
+            this.tabControlApp.Controls.Add(this.tabPageProfile);
+            this.tabControlApp.Controls.Add(this.tabPageFeatures);
+            this.tabControlApp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlApp.Location = new System.Drawing.Point(0, 0);
+            this.tabControlApp.Multiline = true;
+            this.tabControlApp.Name = "tabControlApp";
+            this.tabControlApp.SelectedIndex = 0;
+            this.tabControlApp.Size = new System.Drawing.Size(1209, 594);
+            this.tabControlApp.TabIndex = 54;
+            // 
+            // tabPageProfile
+            // 
+            this.tabPageProfile.Controls.Add(this.labelName);
+            this.tabPageProfile.Controls.Add(this.pictureBoxCoverPhoto);
+            this.tabPageProfile.Controls.Add(this.pictureBoxProfile);
+            this.tabPageProfile.Controls.Add(this.buttonLogout);
+            this.tabPageProfile.Controls.Add(this.buttonLogin);
+            this.tabPageProfile.Location = new System.Drawing.Point(4, 35);
+            this.tabPageProfile.Name = "tabPageProfile";
+            this.tabPageProfile.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageProfile.Size = new System.Drawing.Size(1201, 555);
+            this.tabPageProfile.TabIndex = 0;
+            this.tabPageProfile.Text = "Profile";
+            this.tabPageProfile.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxProfile
+            // 
+            this.pictureBoxProfile.Location = new System.Drawing.Point(18, 96);
+            this.pictureBoxProfile.Name = "pictureBoxProfile";
+            this.pictureBoxProfile.Size = new System.Drawing.Size(79, 78);
+            this.pictureBoxProfile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxProfile.TabIndex = 55;
+            this.pictureBoxProfile.TabStop = false;
+            // 
+            // tabPageFeatures
+            // 
+            this.tabPageFeatures.Location = new System.Drawing.Point(4, 35);
+            this.tabPageFeatures.Name = "tabPageFeatures";
+            this.tabPageFeatures.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageFeatures.Size = new System.Drawing.Size(1201, 555);
+            this.tabPageFeatures.TabIndex = 1;
+            this.tabPageFeatures.Text = "Extra Features";
+            this.tabPageFeatures.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxCoverPhoto
+            // 
+            this.pictureBoxCoverPhoto.Location = new System.Drawing.Point(103, 96);
+            this.pictureBoxCoverPhoto.Name = "pictureBoxCoverPhoto";
+            this.pictureBoxCoverPhoto.Size = new System.Drawing.Size(505, 123);
+            this.pictureBoxCoverPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxCoverPhoto.TabIndex = 56;
+            this.pictureBoxCoverPhoto.TabStop = false;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(13, 238);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(219, 26);
+            this.labelName.TabIndex = 57;
+            this.labelName.Text = "FirstName LastName";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1209, 594);
+            this.Controls.Add(this.tabControlApp);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Facebook";
+            this.tabControlApp.ResumeLayout(false);
+            this.tabPageProfile.ResumeLayout(false);
+            this.tabPageProfile.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCoverPhoto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonLogin;
+		private System.Windows.Forms.Button buttonLogout;
+		private System.Windows.Forms.TabControl tabControlApp;
+		private System.Windows.Forms.TabPage tabPageProfile;
+		private System.Windows.Forms.TabPage tabPageFeatures;
+        private System.Windows.Forms.PictureBox pictureBoxProfile;
+        private System.Windows.Forms.PictureBox pictureBoxCoverPhoto;
+        private System.Windows.Forms.Label labelName;
+    }
+}
+
---
+++ FacebookWinFormsApp/FormMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
+
+namespace BasicFacebookFeatures
+{
+    public partial class FormMain : Form
+    {
+        private const string k_AppID = "832742648143866";
+
+        public FormMain()
+        {
+            InitializeComponent();
+            FacebookWrapper.FacebookService.s_CollectionLimit = 25;
+        }
+
+        FacebookWrapper.LoginResult m_LoginResult;
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("design.patterns");
+
+            if (m_LoginResult == null)
+            {
+                login();
+            }
+        }
+
+        private void login()
+        {
+            m_LoginResult = FacebookService.Login(
+                k_AppID,
+                /// requested permissions:
+                "email",
+                "public_profile"
+                /// add any relevant permissions
+                );
+
+            if (string.IsNullOrEmpty(m_LoginResult.ErrorMessage))
+            {
+                buttonLogin.Text = $"Logged in as {m_LoginResult.LoggedInUser.Name}";
+                buttonLogin.BackColor = Color.LightGreen;
+                pictureBoxProfile.ImageLocation = m_LoginResult.LoggedInUser.PictureNormalURL;
+                buttonLogin.Enabled = false;
+                buttonLogout.Enabled = true;
+            }
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            FacebookService.LogoutWithUI();
+            buttonLogin.Text = "Login";
+            buttonLogin.BackColor = buttonLogout.BackColor;
+            m_LoginResult = null;
+            buttonLogin.Enabled = true;
+            buttonLogout.Enabled = false;
+        }
+    }
+}
